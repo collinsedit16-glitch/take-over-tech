0892ddd725f097234ed32bb3951776a39beb3984
collinsedit16-glitch <collinsedit16@gmail.com>
2026-04-20 15:11:33 +0100

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,11 @@
+function openTab(tabName){
+
+    let tabs=document.getElementsByClassName("tabcontent");
+    
+    for(let i=0;i<tabs.length;i++){
+    tabs[i].style.display="none";
+    }
+    
+    document.getElementById(tabName).style.display="block";
+    
+    }
